34050612082c16dc39690b66b1a7ed0ad4765624
jgomezfl <92111358+jgomezfl@users.noreply.github.com>
2022-02-06 14:17:53 -0500

    Reporte

---
+++ lib/Screens/Reporte.dart
@@ -0,0 +1,49 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:rappi_un/Constants/AllModels.dart';
+import 'package:rappi_un/Constants/FirebaseRepository.dart';
+
+FireRepo _fireRepo = FireRepo();
+
+class Reporte extends StatelessWidget{
+  static const String id = "Reporte";
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: lesCols[5],
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(top: 40, bottom: 40, left: 0, right: 0),
+          child: FutureBuilder(
+              future: _fireRepo.getdatos(),
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else {
+                  print(snapshot);
+                  return Text("data");
+                }
+              }),
+          ),
+        ),
+      );
+  }
+}
+
+class buildStar extends StatelessWidget {
+  const buildStar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Icon(
+      Icons.star,
+      size: 25,
+      color: Colors.yellowAccent[700],
+    );
+  }
+}
